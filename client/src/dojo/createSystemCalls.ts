15870e83869365f11642bda3f377358577c26db6
democritus <ponderingdemocritus@protonmail.com>
2023-11-15 06:05:17 +0100

    cleanup

--- client/src/dojo/createSystemCalls.ts
+++ client/src/dojo/createSystemCalls.ts
@@ -1,14 +1,11 @@
 import { SetupNetworkResult } from "./setupNetwork";
-import { Entity, getComponentValue } from "@dojoengine/recs";
-import { uuid } from "@latticexyz/utils";
 import { ClientComponents } from "./createClientComponents";
-import { updatePositionWithDirection } from "./utils";
 import { MoveSystemProps, SpawnSystemProps } from "./types";
 
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
 export function createSystemCalls(
-    { execute, contractComponents }: SetupNetworkResult,
+    { execute }: SetupNetworkResult,
     { Position, PlayerID }: ClientComponents
 ) {
     const spawn = async (props: SpawnSystemProps) => {
@@ -22,25 +19,7 @@ export function createSystemCalls(
     const move = async (props: MoveSystemProps) => {
         const { signer, direction } = props;
 
-        const currentPosition = getComponentValue(
-            Position,
-            playerId?.id.toString() as Entity
-        ) || { x: 0, y: 0 };
-
-        const newPosition = updatePositionWithDirection(direction, {
-            x: currentPosition["x"],
-            y: currentPosition["y"],
-        });
-
-        const positionId = uuid();
-        Position.addOverride(positionId, {
-            entity: playerId?.id.toString() as Entity,
-            value: {
-                id: 1,
-                x: newPosition["x"],
-                y: newPosition["y"],
-            },
-        });
+        // TODO: Add optimistic updates
 
         try {
             const { transaction_hash } = await execute(
@@ -50,18 +29,14 @@ export function createSystemCalls(
                 [direction]
             );
 
+            // logging the transaction hash
             console.log(
                 await signer.waitForTransaction(transaction_hash, {
                     retryInterval: 100,
                 })
             );
         } catch (e) {
-            console.log(e);
-            // Position.removeOverride(positionId);
-            // Moves.removeOverride(movesId);
-        } finally {
-            // Position.removeOverride(positionId);
-            // Moves.removeOverride(movesId);
+            console.error(e);
         }
     };
 
